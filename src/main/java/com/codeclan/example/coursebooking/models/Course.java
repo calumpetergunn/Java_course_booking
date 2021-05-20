e0ea0133b6b73d3bd245041df7f8631d768a4668
calumpetergunn <calumgunn@gmail.com>
2021-05-20 10:42:29 +0100

    initial commit

---
+++ src/main/java/com/codeclan/example/coursebooking/models/Course.java
@@ -0,0 +1,82 @@
+package com.codeclan.example.coursebooking.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="course_name")
+    private String courseName;
+
+    @Column(name="town")
+    private String town;
+
+    @Column(name="rating")
+    private int rating;
+
+    @OneToMany
+    @JoinColumn(name = "booking", nullable = false)
+    @JsonIgnoreProperties({"courses"})
+    private List<Booking> bookings;
+
+    public Course(String courseName, String town, int rating) {
+        this.courseName = courseName;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
---
+++ src/main/java/com/codeclan/example/coursebooking/models/Customer.java
@@ -0,0 +1,82 @@
+package com.codeclan.example.coursebooking.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="customer_name")
+    private String customerName;
+
+    @Column(name="town")
+    private String town;
+
+    @Column(name="age")
+    private int age;
+
+    @OneToMany
+    @JoinColumn(name = "booking", nullable = false)
+    @JsonIgnoreProperties({"customers"})
+    private List<Booking> bookings;
+
+    public Customer(String customerName, String town, int age){
+        this.customerName = customerName;
+        this.town = town;
+        this.age = age;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Customer(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
